6fee67cb67cb9aa3da78316fca117161175cf190
Farhad <farhadrifaldi@gmail.com>
2022-11-28 05:34:59 +0700

    navbar responsive support

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from "vue";
 import { Link } from "@inertiajs/inertia-vue3";
-
+import { Bars3Icon } from "@heroicons/vue/24/outline";
 const showingNavigationDropdown = ref(false);
 
 const SOCIALS = [
@@ -18,30 +18,54 @@ const SOCIALS = [
     <div class="h-16 flex justify-center bg-primary-base">
       <div class="container flex justify-between items-center">
         <img src="/images/logo-white.png" alt="" />
-        <div class="flex text-white">
+        <div class="text-white hidden md:flex">
           <Link class="py-1 px-7 font-semibold">Home</Link>
           <Link class="py-1 px-7 font-semibold">Article</Link>
           <Link class="py-1 px-7 font-semibold bg-red-600 rounded-xl"
             >Sign Up</Link
           >
         </div>
+        <Bars3Icon
+          class="text-white h-10 w-10 md:hidden"
+          @click="showingNavigationDropdown = !showingNavigationDropdown"
+        />
+      </div>
+      <div
+        :class="`text-white
+        md:hidden
+        absolute
+        w-full
+        bg-primary-base
+        top-16 ${showingNavigationDropdown ? 'block' : 'hidden'}`"
+      >
+        <ul>
+          <li class="py-3 border-b-2 border-primary-lighter">
+            <Link class="py-1 px-7 font-semibold">Home</Link>
+          </li>
+          <li class="py-3 border-b-2 border-primary-lighter">
+            <Link class="py-1 px-7 font-semibold">Article</Link>
+          </li>
+          <li class="py-3 border-b-2 border-primary-lighter">
+            <Link class="py-1 px-7 font-semibold">Sign Up</Link>
+          </li>
+        </ul>
       </div>
     </div>
     <slot />
     <div
-      class="h-14 flex justify-center bg-primary-base text-white font-semibold"
+      class="py-4 flex justify-center bg-primary-base text-white font-semibold"
     >
-      <div class="container flex justify-between items-center">
-        <div class="flex">
+      <div class="container md:flex justify-between items-center">
+        <div class="flex justify-center md:justify-start mb-2">
           <a v-for="social in SOCIALS" :key="social" href="#" target="__blank">
             <img
               :src="`images/${social}`"
               :alt="social"
-              class="h-10 w-10 mr-3"
+              class="h-5 w-5 md:h-10 md:w-10 mr-3"
             />
           </a>
         </div>
-        <div>Copyright 2022@NOVA</div>
+        <div class="text-center md:text-left">Copyright 2022@NOVA</div>
       </div>
     </div>
   </div>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -87,16 +87,17 @@ const TESTIMONIES = [
         from-primary-base
         via-[#505bc7]
         to-primary-lighter
-        py-32
+        md:py-32
+        py-10
         text-white
         flex
         justify-center
       "
     >
-      <div class="container flex flex-row flex-wrap">
-        <div class="basis-1/3 flex items-center">
+      <div class="container flex flex-row flex-wrap px-4 md:px-0">
+        <div class="basis-full md:basis-1/2 flex items-center">
           <div>
-            <p class="text-6xl font-bold mb-4">
+            <p class="text-4xl md:text-6xl font-bold mb-4">
               Nova Content Management System
             </p>
             <p>
@@ -105,15 +106,15 @@ const TESTIMONIES = [
             </p>
           </div>
         </div>
-        <div class="basis-2/3">
+        <div class="md:basis-1/2 hidden md:block">
           <img src="/images/illustration-1.png" alt="illus-1" class="mx-auto" />
         </div>
         <div class="basis-full mt-28 mb-6">
-          <p class="text-2xl font-bold">How its work</p>
+          <p class="text-4xl font-bold">How its work</p>
         </div>
         <div
           v-for="work in WORKS"
-          class="basis-1/3 bg-no-repeat pt-4 px-2"
+          class="basis-full md:basis-1/3 bg-no-repeat pt-4 px-2 mb-10 md:mb-0"
           :key="work.title"
           :style="`background-image: url('${work.image}')`"
         >
@@ -124,17 +125,17 @@ const TESTIMONIES = [
       </div>
     </div>
     <div
-      class="bg-no-repeat h-96 -mt-2 bg-custom-white"
+      class="bg-no-repeat -mt-2 bg-custom-white h-auto"
       style="
         background-image: url('/images/bg-home.png');
         background-size: 100%;
       "
     >
-      <div class="flex flex-row">
+      <div class="flex flex-row flex-wrap">
         <div
           v-for="card in CARDS_CONTENT"
           :key="card.title"
-          class="basis-1/3 py-4 px-10"
+          class="basis-full md:basis-1/4 py-4 px-10"
         >
           <div class="bg-white rounded-md text-center py-10 drop-shadow-lg">
             <img :src="card.image" :alt="card.title" class="mx-auto mb-4" />
@@ -144,15 +145,15 @@ const TESTIMONIES = [
         </div>
       </div>
     </div>
-    <div class="bg-custom-white">
-      <div class="container flex mx-auto px-8 mb-5">
-        <div class="w-1/2">
+    <div class="bg-custom-white md:py-16">
+      <div class="container flex mx-auto py-7 md:py-0 px-8 mb-5">
+        <div class="w-1/2 hidden md:block">
           <img
             src="/images/ececee06-8c52-4513-9355-3e0ea29e8ea5 1 (1).png"
             alt="96b69ed3-d637-48df-bb57-1ce1c111c626 1.png"
           />
         </div>
-        <div class="w-1/2 flex items-center">
+        <div class="w-full md:w-1/2 flex items-center">
           <div>
             <p class="text-3xl font-bold mb-4">
               Easy To Use On All Your device
@@ -169,8 +170,8 @@ const TESTIMONIES = [
           </div>
         </div>
       </div>
-      <div class="container flex mx-auto px-8 mb-5">
-        <div class="w-1/2 flex items-center">
+      <div class="container flex mx-auto py-7 md:py-0 px-8 mb-5">
+        <div class="w-full md:w-1/2 flex items-center">
           <div>
             <p class="text-3xl font-bold mb-4">
               Manage All Content With Creativity
@@ -186,21 +187,21 @@ const TESTIMONIES = [
             </p>
           </div>
         </div>
-        <div class="w-1/2">
+        <div class="w-1/2 hidden md:block">
           <img
             src="/images/96b69ed3-d637-48df-bb57-1ce1c111c626 1.png"
             alt="96b69ed3-d637-48df-bb57-1ce1c111c626 1.png"
           />
         </div>
       </div>
-      <div class="container flex mx-auto px-8 mb-5">
-        <div class="w-1/2">
+      <div class="container flex mx-auto py-7 md:py-0 px-8 mb-5">
+        <div class="w-1/2 hidden md:block">
           <img
             src="/images/793b60c6-6939-4938-9eea-67dbf61e1e9c 1.png"
             alt="793b60c6-6939-4938-9eea-67dbf61e1e9c 1.png"
           />
         </div>
-        <div class="w-1/2 flex items-center">
+        <div class="w-full md:w-1/2 flex items-center">
           <div>
             <p class="text-3xl font-bold mb-4">
               The Best Content Management System
@@ -219,12 +220,12 @@ const TESTIMONIES = [
       </div>
     </div>
     <div class="bg-primary-light px-4 py-8">
-      <p class="text-3xl text-white font-bold">Client Testimony</p>
-      <div class="flex flex-row">
+      <p class="text-3xl text-white font-bold text-center md:text-left mb-10">Client Testimony</p>
+      <div class="flex flex-row flex-wrap">
         <div
           v-for="testimony in TESTIMONIES"
           :key="testimony.title"
-          class="basis-1/3 py-4 px-2"
+          class="basis-full md:basis-1/3 py-4 px-2"
         >
           <div class="bg-white rounded-md text-center py-5 drop-shadow-lg px-4">
             <div class="flex mb-4">
